22eaa602d2405d9ca711cd35bea3469d91cdf84a
Yeferal <yefercuber20@gmail.com>
2023-10-31 11:22:21 -0600

    add bash agent zabbix

--- agent_zabbix.sh
+++ agent_zabbix.sh
@@ -1,16 +1,44 @@
 # !/bin/bash
 
 # Descarga el agente de Zabbix
-# Para Debian
-sudo apt-get update
-sudo apt-get install zabbix-agent
-
-# Para CentosOS
-# sudo yum install zabbix-agent
 
 
-sudo nano /etc/zabbix/zabbix_agentd.conf
+
+# Mostrar las opciones al usuario
+echo "Elige una opción:"
+echo "1. Opción 1"
+echo "2. Opción 2"
+
+# Leer la entrada del usuario
+read opcion
+
+# Utilizar una estructura case para manejar las opciones
+case $opcion in
+    1)
+        echo "Debian"
+        # Para Debian
+        sudo apt-get update
+        sudo apt-get install zabbix-agent
+        sudo nano /etc/zabbix/zabbix_agentd.conf
+        sudo systemctl start zabbix-agent
+        sudo systemctl enable zabbix-agent
+        ;;
+    2)
+        echo "CentosOS"
+        # Para CentosOS
+        sudo yum install zabbix-agent
+        sudo nano /etc/zabbix/zabbix_agentd.conf
+        sudo systemctl start zabbix-agent
+        sudo systemctl enable zabbix-agent
+        ;;
+    *)
+        echo "Opción no válida."
+        ;;
+esac
 
 
-sudo systemctl start zabbix-agent
-sudo systemctl enable zabbix-agent
+# sudo nano /etc/zabbix/zabbix_agentd.conf
+
+
+# sudo systemctl start zabbix-agent
+# sudo systemctl enable zabbix-agent
